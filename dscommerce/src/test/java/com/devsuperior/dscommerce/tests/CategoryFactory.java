fc41ddb75d8b60d205a29ad21992facab087f2ce
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2024-12-12 16:46:41 -0300

    ProductControllerIT

--- dscommerce/src/test/java/com/devsuperior/dscommerce/tests/CategoryFactory.java
+++ dscommerce/src/test/java/com/devsuperior/dscommerce/tests/CategoryFactory.java
@@ -5,7 +5,7 @@ import com.devsuperior.dscommerce.entities.Category;
 public class CategoryFactory {
 
     public static Category createCategory(){
-        return new Category(1L, "Games");
+        return new Category(2L, "Games");
     }
 
     public static Category createCategory(Long id, String name){
