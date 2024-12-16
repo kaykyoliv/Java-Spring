cd040a9b19fe4ce9e1cafaac14045a61a4a3a899
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2024-12-16 16:00:36 -0300

    EmailService, SendGrid

--- Java-Spring/integrations/src/main/java/com/kaykyoliveira/integrations/controllers/EmailController.java
+++ Java-Spring/integrations/src/main/java/com/kaykyoliveira/integrations/controllers/EmailController.java
@@ -1,6 +1,8 @@
 package com.kaykyoliveira.integrations.controllers;
 
 import com.kaykyoliveira.integrations.dto.EmailDTO;
+import com.kaykyoliveira.integrations.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/emails")
 public class EmailController {
 
+    @Autowired
+    private EmailService service;
     @PostMapping
     public ResponseEntity<Void> send(@RequestBody EmailDTO dto){
+        service.sendEmail(dto);
         return ResponseEntity.noContent().build();
     }
 }
